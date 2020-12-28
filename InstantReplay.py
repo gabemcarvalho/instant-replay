bf1dc0aaf0be37d956983abe2350fa3c14c6f9aa
Gabriel Carvalho <stargazer.carvalho@gmail.com>
2020-12-28 17:18:06 -0500

    update variable naming for clarity

--- InstantReplay.py
+++ InstantReplay.py
@@ -5,127 +5,137 @@ import keyboard
 import threading
 import json
 
-# load settings
+# constants
+LIVE_VIDEO_POS = (20, 40)
+SAVE_DIRECTORY = "clips/"
+
+# global variables
+is_playing_looped = False
+looped_video_capture = None
+looped_video_current_frame_number = 0
+saving = False
+
+# load settings (these should probably be defined in a separate Settings)
 with open("settings.json") as settingsFile:
     settings = json.load(settingsFile)
 
-C_BUFFER_SIZE = settings["bufferSize"]
-C_FPS = settings["fps"]
-C_LIVE_DELAY_SECONDS = settings["videoDelaySeconds"]
-C_LOOP_SECONDS = settings["loopedVideoLength"]
-C_CAMERA_ROTATIONS = settings["cameraRotations"]
+BUFFER_SIZE = settings["fps"] * (1 + settings["videoDelaySeconds"] + settings["loopedVideoLength"])
+TARGET_FPS = settings["fps"]
+LIVE_DELAY_SECONDS = settings["videoDelaySeconds"]
+LOOP_SECONDS = settings["loopedVideoLength"]
+CAMERA_ROTATIONS = settings["cameraRotations"]
 
-C_LIVE_VIDEO_X = 20
-C_LIVE_VIDEO_Y = 40
+def main():
 
-frameBuffer = []
-pFront = 0
-pLive = 0
-playingLooped = False
-lv = None
-lvName = ""
-frameCounter = 0
-
-blackFrame = np.zeros((720, 1280, 3), np.uint8)
-
-cap = cv2.VideoCapture(settings["cameraIndex"])
-
-spacePressedLast = False
-saving = False
-
-cv2.namedWindow("frame", cv2.WINDOW_NORMAL)
-cv2.setWindowProperty("frame", cv2.WND_PROP_ASPECT_RATIO, cv2.WINDOW_KEEPRATIO)
-if settings["fullscreen"] == True:
-    cv2.setWindowProperty("frame", cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
-
-lastTime = time.time()
-k = 0
-fps = 0
-
-def saveClip(startIndex, name, width, height, frameLength):
-    global lv
-    global playingLooped
-    saving = True
-    print("saving video...")
-    fourcc = cv2.VideoWriter_fourcc(*"XVID")
-    out = cv2.VideoWriter(name + ".avi", fourcc, C_FPS, (width, height))
-    for i in range(frameLength):
-        index = (C_BUFFER_SIZE + startIndex + i) % C_BUFFER_SIZE
-        if index > len(frameBuffer) - 1:
-            print("couldn't save video!")
-            out.release()
-            saving = False
-            break
-        saveFrame = frameBuffer[index]
-        out.write(saveFrame)
-    out.release()
-    print("saved " + name + ".avi, looping")
-    playingLooped = True
-    if lv != None:
-        lv.release()
-    lvName = name + ".avi"
-    lv = cv2.VideoCapture(lvName)
-    frameCounter = 0
-    saving = False
-
-while(True):
-    ret, rawFrame = cap.read()
-    rotFrame = np.rot90(rawFrame, C_CAMERA_ROTATIONS).copy()
-
-    if len(frameBuffer) < C_BUFFER_SIZE:
-        frameBuffer.append(rotFrame)
-    else:
-        frameBuffer[pFront] = rotFrame
+    global looped_video_current_frame_number
     
-    pLive = (C_BUFFER_SIZE + pFront - C_FPS * C_LIVE_DELAY_SECONDS) % C_BUFFER_SIZE
-    if pLive > len(frameBuffer) - 1: pLive = 0
-    liveFrame = frameBuffer[pLive]
+    frame_buffer = []
+    front_frame_number = 0
+    showing_frame_number = 0
 
-    pFront = (pFront + 1) % C_BUFFER_SIZE
+    black_frame = np.zeros((720, 1280, 3), np.uint8)
 
-    displayFrame = blackFrame.copy()
-    displayFrame[C_LIVE_VIDEO_Y:C_LIVE_VIDEO_Y+liveFrame.shape[0], C_LIVE_VIDEO_X:C_LIVE_VIDEO_X+liveFrame.shape[1], 0:3] = liveFrame
+    main_video_capture = cv2.VideoCapture(settings["cameraIndex"])
 
-    thisTime = time.time()
-    k += 1
-    if thisTime - lastTime >= 1:
-        fps = k
-        k = 0
-        lastTime = thisTime
+    spacePressedLast = False
 
-    if keyboard.is_pressed("space") and spacePressedLast == False:
-        if saving == True:
-            print("Already saving a video!")
+    cv2.namedWindow("frame", cv2.WINDOW_NORMAL)
+    cv2.setWindowProperty("frame", cv2.WND_PROP_ASPECT_RATIO, cv2.WINDOW_KEEPRATIO)
+    if settings["fullscreen"] == True:
+        cv2.setWindowProperty("frame", cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
+
+    last_time = time.time()
+    frames_in_last_second = 0
+    real_fps = 0
+
+    while(True):
+        ret, raw_frame = main_video_capture.read()
+        rotated_frame = np.rot90(raw_frame, CAMERA_ROTATIONS).copy()
+
+        if len(frame_buffer) < BUFFER_SIZE:
+            frame_buffer.append(rotated_frame)
         else:
-            # save a video in another thread
-            length = C_FPS * C_LOOP_SECONDS;
-            startI = (C_BUFFER_SIZE + pLive - length) % C_BUFFER_SIZE
-            saveThread = threading.Thread(target=saveClip, args=(startI, str(int(lastTime)), liveFrame.shape[1], liveFrame.shape[0], length))
-            saveThread.start()
+            frame_buffer[front_frame_number] = rotated_frame
+        
+        showing_frame_number = (BUFFER_SIZE + front_frame_number - TARGET_FPS * LIVE_DELAY_SECONDS) % BUFFER_SIZE
+        if showing_frame_number > len(frame_buffer) - 1:
+            showing_frame_number = 0
+        showing_frame = frame_buffer[showing_frame_number]
 
-    spacePressedLast = keyboard.is_pressed("space");
+        front_frame_number = (front_frame_number + 1) % BUFFER_SIZE
 
-    if playingLooped == True and lv != None:
-        if lv.isOpened():
-            ret, rawLoopFrame = lv.read()
-            frameCounter += 1
-            if frameCounter == lv.get(cv2.CAP_PROP_FRAME_COUNT):
-                    lv.set(cv2.CAP_PROP_POS_FRAMES, 0)
-                    frameCounter = 0
-            if ret == True:
-                displayFrame[C_LIVE_VIDEO_Y:C_LIVE_VIDEO_Y+rawLoopFrame.shape[0], C_LIVE_VIDEO_X+500:C_LIVE_VIDEO_X+500+rawLoopFrame.shape[1], 0:3] = rawLoopFrame
+        display_frame = black_frame.copy()
+        display_frame[LIVE_VIDEO_POS[1]:LIVE_VIDEO_POS[1]+showing_frame.shape[0], LIVE_VIDEO_POS[0]:LIVE_VIDEO_POS[0]+showing_frame.shape[1], 0:3] = showing_frame
 
-    # cv2.rectangle(displayFrame, (0, 0), (480, 20), (0, 0, 0), -1) # note: thickness of -1 fills rectangle
-    cv2.putText(displayFrame, "Frames: " + str(len(frameBuffer)), (10, 15), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255))
-    cv2.putText(displayFrame, "Front: " + str(pFront), (150, 15), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255))
-    cv2.putText(displayFrame, "Showing: " + str(pLive), (250, 15), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255))
-    cv2.putText(displayFrame, "FPS: " + str(fps), (400, 15), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255))
-    cv2.putText(displayFrame, "PRESS 'Q' TO QUIT", (500, 15), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255))
-    cv2.putText(displayFrame, "PRESS 'SPACE' TO SAVE 10 SECONDS", (700, 15), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255))
+        current_time = time.time()
+        frames_in_last_second += 1
+        if current_time - last_time >= 1:
+            real_fps = frames_in_last_second
+            frames_in_last_second = 0
+            last_time = current_time
 
-    if ret: cv2.imshow("frame", displayFrame)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+        if keyboard.is_pressed("space") and spacePressedLast == False:
+            if saving == True:
+                print("Already saving a video!")
+            else:
+                # save a video in another thread
+                start_index = (BUFFER_SIZE + showing_frame_number - TARGET_FPS * LOOP_SECONDS) % BUFFER_SIZE
+                saveThread = threading.Thread(target=save_clip, args=(frame_buffer, start_index, str(int(last_time)), showing_frame.shape[1], showing_frame.shape[0]))
+                saveThread.start()
 
-cap.release()
-cv2.destroyAllWindows()
+        spacePressedLast = keyboard.is_pressed("space")
+
+        if is_playing_looped == True and looped_video_capture != None:
+            if looped_video_capture.isOpened():
+                ret, rawLoopFrame = looped_video_capture.read()
+                looped_video_current_frame_number += 1
+                if looped_video_current_frame_number == looped_video_capture.get(cv2.CAP_PROP_FRAME_COUNT):
+                        looped_video_capture.set(cv2.CAP_PROP_POS_FRAMES, 0)
+                        looped_video_current_frame_number = 0
+                if ret == True:
+                    display_frame[LIVE_VIDEO_POS[1]:LIVE_VIDEO_POS[1]+rawLoopFrame.shape[0], LIVE_VIDEO_POS[0]+500:LIVE_VIDEO_POS[0]+500+rawLoopFrame.shape[1], 0:3] = rawLoopFrame
+
+        # cv2.rectangle(display_frame, (0, 0), (480, 20), (0, 0, 0), -1) # note: thickness of -1 fills rectangle
+        cv2.putText(display_frame, "Frames: " + str(len(frame_buffer)), (10, 15), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255))
+        cv2.putText(display_frame, "Front: " + str(front_frame_number), (150, 15), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255))
+        cv2.putText(display_frame, "Showing: " + str(showing_frame_number), (250, 15), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255))
+        cv2.putText(display_frame, "FPS: " + str(real_fps), (400, 15), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255))
+        cv2.putText(display_frame, "PRESS 'Q' TO QUIT", (500, 15), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255))
+        cv2.putText(display_frame, "PRESS 'SPACE' TO SAVE 10 SECONDS", (700, 15), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255))
+
+        if ret: cv2.imshow("frame", display_frame)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    main_video_capture.release()
+    cv2.destroyAllWindows()
+
+def save_clip(frame_buffer, start_index, name, width, height):
+        global looped_video_capture
+        global is_playing_looped
+        global saving
+        global looped_video_current_frame_number
+        saving = True
+        print("saving video...")
+        fourcc = cv2.VideoWriter_fourcc(*"XVID")
+        out = cv2.VideoWriter(SAVE_DIRECTORY + name + ".avi", fourcc, TARGET_FPS, (width, height))
+        for i in range(TARGET_FPS * LOOP_SECONDS):
+            index = (BUFFER_SIZE + start_index + i) % BUFFER_SIZE
+            if index > len(frame_buffer) - 1:
+                print("couldn't save video!")
+                out.release()
+                saving = False
+                break
+            save_frame = frame_buffer[index]
+            out.write(save_frame)
+        out.release()
+        print("saved " + name + ".avi, looping")
+        is_playing_looped = True
+        if looped_video_capture != None:
+            looped_video_capture.release()
+        looped_video_capture = cv2.VideoCapture(SAVE_DIRECTORY + name + ".avi")
+        looped_video_current_frame_number = 0
+        saving = False
+
+if __name__ == '__main__':
+    main()
